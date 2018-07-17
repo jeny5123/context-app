d40a0cfee49f3a0008c433f62cfb1efd1d3855bb
Chau <chauj@vimny-chauJ-lm.local>
2018-07-17 14:29:42 -0400

    redux stuff

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,9 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+/* import PropTypes from 'prop-types';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux'; */
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
